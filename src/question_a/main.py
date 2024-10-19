bbf7f4b32dcf89da83f6cf68232f4354162bb32a
Github11111111111111 <ralph.stjohniii@g.austincc.edu>
2024-10-19 19:49:52 +0000

    Midterm

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,25 @@
 #add import
+def get_bonus_pay_amount(sales):
+    if sales < 0 or sales > 1999:
+        return "Invalid arguments"
+    
+    if sales < 500:
+        bonus_percentage = 0.05
+    elif sales < 1000:
+        bonus_percentage = 0.06
+    elif sales < 1500:
+        bonus_percentage = 0.07
+    else:  # sales is between 1500 and 1999
+        bonus_percentage = 0.08
+
+    bonus_amount = sales * bonus_percentage
+    return bonus_amount
+
+# Prompt the user for sales amount
+try:
+    sales_input = float(input("Enter the sales amount: "))
+    bonus = get_bonus_pay_amount(sales_input)
+    print(f"The bonus pay amount for sales of {sales_input} is: {bonus}")
+except ValueError:
+    print("Please enter a valid number.")
+200
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,20 @@
 #write functions here, don't add input('') statements here!
-def test_config():
-    return True
+def get_bonus_pay_amount(sales):
+    if sales < 0 or sales > 1999:
+        return "Invalid arguments"
+    
+    if sales < 500:
+        bonus_percentage = 0.05
+    elif sales < 1000:
+        bonus_percentage = 0.06
+    elif sales < 1500:
+        bonus_percentage = 0.07
+    else:  
+        bonus_percentage = 0.08
+
+    bonus_amount = sales * bonus_percentage
+    return bonus_amount
+
+
+
+
